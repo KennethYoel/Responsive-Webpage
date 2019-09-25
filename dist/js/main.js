1eb54021ffc069fd0a5529be0cdd0a9d1f272893
Kenneth Gutierrez <kenneth.joel@live.com>
2019-09-25 04:54:20 -0400

    Added fixed menu and mobile menu.

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,18 +13,18 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show')); // forEach is a high order array method.
+    menu.classList.add('showOverlay');
+    menuNav.classList.add('showOverlay');
+    menuBranding.classList.add('showOverlay');
+    navItems.forEach(item => item.classList.add('showOverlay')); // forEach is a high order array method.
     // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove('close'); // All the opposite of the code above.
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+    menu.classList.remove('showOverlay');
+    menuNav.classList.remove('showOverlay');
+    menuBranding.classList.remove('showOverlay');
+    navItems.forEach(item => item.classList.remove('showOverlay'));
     // Set Menu State
     showMenu = false;
   }
